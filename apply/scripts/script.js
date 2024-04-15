a5fdf13dd6d41fec71484b8552d737f5495d2ed6
TlucasX5 <sl93@sunyorange.edu>
2024-04-15 16:11:32 -0400

    Apply

---
+++ apply/scripts/script.js
@@ -0,0 +1,14 @@
+
+/* Student Name: Suzanne Lucas
+File Name: template.html
+Date: 2/12/2024
+File: script.js
+*/
+
+/* Function to display content */
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
